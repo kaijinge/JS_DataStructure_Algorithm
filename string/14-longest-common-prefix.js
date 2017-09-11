d21636583c228ff992c34227353ee6298114f1d2
Jinge Kai <kaijinge@gmail.com>
2017-09-11 11:17:25 +0800

    Update 14-longest-common-prefix.js

--- string/14-longest-common-prefix.js
+++ string/14-longest-common-prefix.js
@@ -15,9 +15,9 @@
  	//以第一个元素为例，遍历从第一个元素到len，与数组中其他元素判断
  	var ans = '';
  	for(var i = 0; i < len; i++){
- 		var temp = strs[0][i];
-
- 		var flag = strs.every(function(item){
+    var temp = strs[0][i];
+   
+ 		 var flag = strs.every(function(item){
  			return item[i] === temp;
  		});
 
